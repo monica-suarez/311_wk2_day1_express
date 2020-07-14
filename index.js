2cb2d208ca7c0989055b3d90c1f249ae9874cf41
monica-suarez <shutterbugmo@gmail.com>
2020-07-13 20:56:54 -0500

    part 1 and done

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ const port = process.env.PORT || 4000
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
-
-
+app.get('./users', (req, res) =>{
+  return res.json(users);
+});
+app.get('./users/1', (req, res)=>{
+  return res.json(users[0]);
+})
+app.get('./users/:userId', (req, res)=>{
+  const id = req.params.userId;
+  console.log(id); //console.log for visual representation
+  const filteredUsers = users.filter(user => user._id === Number(id))
+  console.log(filteredUsers);
+  res.json(filteredUsers);
+})
 /* END - create routes here */
 
 app.listen(port, () => 
