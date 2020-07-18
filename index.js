53881404a3ca69631315f26731a3a554a2a09b09
monica-suarez <shutterbugmo@gmail.com>
2020-07-18 14:49:21 -0500

    by god i think this works. think.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 const express = require('express')
-const uuid = require('uuid');
+
+//adds random user id to new user
+// const uuid = require('uuid');
+
+
+
 const app = express()
 const port = process.env.PORT || 4000
 const { users } = require('./state')
+
 
 //body parser middleware
 app.use(express.json());
@@ -15,9 +21,9 @@ app.use(express.urlencoded({extended: false}));
 app.get('/users', (req, res) =>{
   return res.json(users);
 });
-app.get('/users/1', (req, res)=>{
-  return res.json(users[0]);
-});
+// app.get('/users/1', (req, res)=>{
+//   return res.json(users[0]);
+// });
 app.get('/users/:userId', (req, res)=>{
   const id = req.params.userId;
   console.log(id); //console.log for visual representation
@@ -28,8 +34,9 @@ app.get('/users/:userId', (req, res)=>{
 
 //create new user
 app.post('/users', (req, res)=>{
+  let counter = users.length;
   const newUser = {
-    _id: uuid.v4(), 
+    _id: (counter += 1), 
     name: req.body.name,
     occupation: req.body.occupation, 
     avatar: req.body.avatar
@@ -41,50 +48,50 @@ app.post('/users', (req, res)=>{
   res.json(newUser);
 });
 
-//update first user 
-app.put('/users/1', (req, res)=>{
-  if(users[0]){
-    const updateUser = req.body;
-    user.name = updateUser.name ? updateUser.name : user.name;
-    user.occupation = updateUser.occupation ? updateUser.occupation : user.occupation;
-    user.avatar = updateUser.avatar ? updateUser.avatar : user.avatar;
-    res.json(updateUser);
-  }
-});
-
-//delete first user
-app.delete('/users/1', (req, res)=>{
-  
-})
-
-//update current user -any user
-// app.put('/users/:userId', (req, res)=>{
-//   const id = req.params.userId;
-//   const filteredUsers = users.filter(user => user._id === Number(id))
-//   if(filteredUsers){
-//     const updateUser = req.body;
-//     users.forEach(user => {
-//       if(user._id === Number(id)){
-//         user.name = updateUser.name ? updateUser.name : user.name;
-//         user.occupation = updateUser.email ? updateUser.email : user.email;
-//         user.avatar = updateUser.avatar ? updateUser.avatar : user.avatar;
-//         res.json(updateUser);
-//       }else{
-//         res.status(400).json({ msg: 'No member with the id of ${req.params.userId}' })
-//       }
-//     })
+// // update first user 
+// app.put('/users/1', (req, res)=>{
+//   const user = users[0];
+//   const updateUser = req.body;
+//   if(users[0]){
+//     user.name = updateUser.name ? updateUser.name : user.name;
+//     user.occupation = updateUser.occupation ? updateUser.occupation : user.occupation;
+//     user.avatar = updateUser.avatar ? updateUser.avatar : user.avatar;
+//     res.json(updateUser);
 //   }
-//   });
+// });
+
+// // delete first user
+// app.delete('/users/1', (req, res)=>{
+//     return res.send({ msg: 'deleted' });
+// });
+
+// update current user -any user
+app.put('/users/:userId', (req, res)=>{
+  const id = req.params.userId;
+  const filteredUsers = users.filter(user => user._id === Number(id))
+  if(filteredUsers){
+    const updateUser = req.body;
+    users.forEach(user => {
+      if(user._id === Number(id)){
+        user.name = updateUser.name ? updateUser.name : user.name;
+        user.occupation = updateUser.email ? updateUser.email : user.email;
+        user.avatar = updateUser.avatar ? updateUser.avatar : user.avatar;
+        res.json(updateUser);
+      }
+    })
+  }
+  });
 
   //delete user - any user
-//   app.delete('/users/:userId', (req, res)=>{
-//     const id = req.params.userId;
-//     const filteredUsers = users.filter(user => user._id === Number(id))
-//     if(filteredUsers){
-//       res.json(users.filter(user => user._id !== Number(id)))
-//       res.send({ msg: 'Deleted' });
-//     }
-// });
+  app.delete('/users/:userId', (req, res)=>{
+    const id = req.params.userId;
+    const filteredUsers = users.find(user => user._id === Number(id))
+    if(filteredUsers){
+      // res.json(users.filter(user => user._id !== Number(id)))
+      filteredUsers.isActive = false;
+      res.send({ msg: 'Deleted' });
+    }
+});
 
 /* END - create routes here */
 
